eed64a37db3c92e2ed7b2de90596e5f8fabdc8ae
fiansyahm <fiansyahmm@gmail.com>
2021-11-26 15:39:08 +0700

    .

---
+++ CameraApp/app/src/main/java/com/example/cameraapp/CheckActivity.java
@@ -0,0 +1,25 @@
+package com.example.cameraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class CheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check);
+
+//        baru=findViewById(R.id.pindah_camera);
+//        baru.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(getBaseContext(), CameraActivity.class));
+//            }
+//        });
+
+    }
+}
--- CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.java
+++ CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -58,13 +58,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-            baru=findViewById(R.id.pindah_camera);
-            baru.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getBaseContext(), CameraActivity.class));
-            }
-        });
+
 
         }
 }
