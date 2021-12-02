7355a149d812d25a007f6b74476f8912105a412d
fiansyahm <fiansyahmm@gmail.com>
2021-12-02 11:25:19 +0700

    .

--- CameraApp/app/src/main/java/com/example/cameraapp/AttandenceActivity.java
+++ CameraApp/app/src/main/java/com/example/cameraapp/AttandenceActivity.java
@@ -6,8 +6,15 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class AttandenceActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class AttandenceActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +23,14 @@ public class AttandenceActivity extends AppCompatActivity {
         Spinner spinner = (Spinner) findViewById(R.id.spinnerWorkCode);
         ArrayAdapter<String> AdapterList = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,list);
         spinner.setAdapter(AdapterList);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng ITS = new LatLng(-7.2819705,112.795323);
+        mMap.addMarker(new MarkerOptions().position(ITS).title("Marker in ITS"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(ITS));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ITS,15));
     }
 }
