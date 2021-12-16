ab71e59a3fa6372082a91e5be213b1019606cf3a
fiansyahm <fiansyahmm@gmail.com>
2021-12-16 12:19:52 +0700

    .

--- finalproject/app/src/main/java/com/salmanarif/tugasppb/DetailAttendanceActivity.java
+++ finalproject/app/src/main/java/com/salmanarif/tugasppb/DetailAttendanceActivity.java
@@ -2,6 +2,7 @@ package com.salmanarif.tugasppb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class DetailAttendanceActivity extends AppCompatActivity {
@@ -10,5 +11,16 @@ public class DetailAttendanceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_attendance);
+        Bundle extras = getIntent().getExtras();
+        extras.getString("id");
+        extras.getString("nama");
+        extras.getString("posisi");
+        extras.getString("waktu");
+        extras.getString("workfromhome");
+        extras.getString("statusabsen");
+        extras.getString("photo");
+        extras.getString("signature");
+
+
     }
 }
